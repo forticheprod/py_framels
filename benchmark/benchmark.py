304ad450bc50f466abc175bc9bf83847904e87b3
Philippe Llerena <philippe.llerena@forticheprod.com>
2024-01-21 20:38:29 +0100

    Update benchmark.py with new test cases

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -5,22 +5,24 @@ import py_framels
 import matplotlib.pyplot as plt
 
 
-def benchmark_pyseq(x: list[int], name: str):
+def benchmark_pyseq(x: list[int]):
+    name = f"bench_{str(x[-1])}"
     y1 = list()
     y2 = list()
     for i in x:
         data_set: list[str] = list()
-        for i in range(0, i):
-            data_set.append(f"beauty_left.{str(i).zfill(6)}.exr")
-
-        now = datetime.now()
-        s = Sequence(data_set)
-        y2.append((datetime.now() - now).total_seconds())
-
+        for aov in ["Beauty", "Specular", "IndirectDiffuse", "SSS"]:
+            for i in range(0, i):
+                data_set.append(f"{aov}_left.{str(i).zfill(6)}.exr")
         now = datetime.now()
         py_framels.py_basic_listing(data_set)
         y1.append((datetime.now() - now).total_seconds())
 
+        now = datetime.now()
+        s = Sequence(data_set)
+        y2.append((datetime.now() - now).total_seconds())
+    print("py_framels", y1)
+    print("pyseq", y2)
     plt.plot(x, y1, label="py_framels")
     plt.plot(x, y2, label="pyseq")
     plt.xlabel("Number of string")
@@ -31,7 +33,8 @@ def benchmark_pyseq(x: list[int], name: str):
     plt.close()
 
 
-x = [1, 2, 5, 10, 50, 100]
-benchmark_pyseq(x, "bench_100")
-x = [100, 1000, 10000, 50000, 100000]
-benchmark_pyseq(x, "bench_100000")
+if __name__ == "__main__":
+    x = [1, 2, 5, 10, 50, 100]
+    benchmark_pyseq(x)
+    x = [100, 1000, 20000, 25000]
+    benchmark_pyseq(x)
